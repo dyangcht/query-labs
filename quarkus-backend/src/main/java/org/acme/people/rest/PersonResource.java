1741a20ab868681055a17567b197adc269783150
David <dyangcht@aim.com>
2021-10-11 19:10:25 +0800

    update maxSurge

--- quarkus-backend/src/main/java/org/acme/people/rest/PersonResource.java
+++ quarkus-backend/src/main/java/org/acme/people/rest/PersonResource.java
@@ -109,7 +109,7 @@ public class PersonResource {
 
     @Transactional
     void onStart(@Observes StartupEvent ev) {
-        for (int i = 0; i < 1100; i++) {
+        for (int i = 0; i < 1500; i++) {
             String name = CuteNameGenerator.generate();
             LocalDate birth = LocalDate.now().plusWeeks(Math.round(Math.floor(Math.random() * 40 * 52 * -1)));
             EyeColor color = EyeColor.values()[(int) (Math.floor(Math.random() * EyeColor.values().length))];
